17a2619092255c3e28b0bc105e10c0e465526880
Duongsiuuu <vuquangduong29042004@gmail.com>
2025-01-06 09:06:10 +0700

    Testing

---
+++ src/test/java/com/example/testing_course/TestingCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testing_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
